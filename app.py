97ff5525f1632fdfffdaeb1e31da2ddb03fd354e
Jovani Guerrero <87553650+JOVSABIO@users.noreply.github.com>
2025-10-06 12:04:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,61 @@
 from pathlib import Path
 import streamlit as st
 
-
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 profile_pic = current_dir / "assets" / "m8vilidad.png"
 
 # --- PAGE SETUP ---
+
 Settings_page = st.Page(
-    page="views/indice.py",
+    page = "views/indice.py",
     title="Indice",
     icon="📄",
     default=True,
 )
 
 Components = st.Page(
-    page="views/componentes.py",
+    page = "views/componentes.py",
     title="Componentes",
     icon="🛠️"
 )
 
 Integrador = st.Page(
-    page="views/integrador.py",
-    title="Mapa Interactivo",
+    page = "views/integrador.py",
+    title="Integrador",
     icon="🌎"
 )
 
-# --- NAVIGATION SETUP [WITH SECTIONS] ---
-pg = st.navigation(
-    {
-        "Introducción": [Settings_page],
-        "Temas": [Components, Integrador],
-    }
+Insights_page = st.Page(
+    page="views/insights.py", 
+    title="Insights",
+    icon="🔍"
 )
 
+Machine_page = st.Page(
+    page="views/ml.py", 
+    title="Machine Learning",
+    icon="🤖"
+)
+
+# --- NAVIGATION SETUP [WITH SECTIONS] ---
+
+pg = st.navigation(
+    {
+        "Introducción":[Settings_page],
+        "Temas": [Components, Integrador],
+         "Análisis": [Insights_page],
+        "Machine Learning": [Machine_page]
+    }
+)  
+
 # --- SHARED ON ALL PAGES ---
-st.image(str(profile_pic), width=1800)
+profile_pic = current_dir / "assets" / "m8vilidad.png"
+st.image(profile_pic, width=1800)
 
 st.sidebar.text("Made with ❤️ by GRUPO 8")
 
 # --- RUN NAVIGATION ---
-pg.run()
 
+pg.run()
